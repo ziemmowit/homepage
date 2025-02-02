0fb175521a3e5fe0b30880367a9e75a3ac3c2abf
ZiemowitR <ziemmowit@gmail.com>
2025-02-02 01:21:40 +0100

    Align-to right for both -> equalWidth true/false. Font0-size to PX

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -14,19 +14,23 @@ export const Logo = ({ topText, bottomText, equalWidth = false }: LogoProps) => 
   const bottomRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
+    console.log("topText", topText);
     if (equalWidth && topRef.current && bottomRef.current) {
       const bottomWidth = bottomRef.current.offsetWidth;
       const topEl = topRef.current;
       let fontSize = parseInt(window.getComputedStyle(topEl).fontSize);
-      
+      console.log("bottomWidth", bottomWidth);
+      console.log("topEl.offsetWidth", topEl.offsetWidth);
       while (topEl.offsetWidth < bottomWidth && fontSize < 100) {
         fontSize += 1;
         topEl.style.fontSize = `${fontSize}px`;
+        console.log("1", topEl.offsetWidth, bottomWidth);
       }
       
       while (topEl.offsetWidth > bottomWidth && fontSize > 12) {
         fontSize -= 1;
         topEl.style.fontSize = `${fontSize}px`;
+        console.log("2", topEl.offsetWidth, bottomWidth);
       }
     }
   }, [equalWidth, topText, bottomText]);
